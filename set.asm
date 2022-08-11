27fa10d6169817f5a9ac35ffd99081e33c693f32
sky line <32512284+linesky@users.noreply.github.com>
2022-08-11 20:02:45 +0100

    address page memory video

--- set.asm
+++ set.asm
@@ -1,13 +1,24 @@
 ;http://www.6502asm.com/
 
-
+lda #$2
+sta $1
+lda #$0
+sta $0
+ford:
+again:
 lda #$5
 ldx #$0
-again:
-sta $200,x
+sta ($0,x)
+ldx $0
 inx
-cmp $ff,x
+stx $0
+cpx #$0
 bne again
+ldx $1
+inx
+stx $1
+cpx #$6
+bne ford
 hlt:
 jmp hlt
 rts
