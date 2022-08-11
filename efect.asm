231f28f391394fd7afbedd2186263cafe8d67246
sky line <32512284+linesky@users.noreply.github.com>
2022-08-11 20:57:20 +0100

    video efect assembly

--- efect.asm
+++ efect.asm
@@ -1,17 +1,33 @@
 ;http://www.6502asm.com/
 
-
+lda #$2
+sta $1
+lda #$0
+sta $0
+ldy #$32
+again:
 lda #$5
 ldx #$0
-again:
-sta $200,x
-sta $300,x
-sta $400,x
-sta $500,x
-inx
-inx
-cmp $ff,x
+sta ($0,x)
+lda $0
+clc
+adc #$21
+bcc again2
+sta $0
+clc
+lda $1
+clc
+adc #$1
+sta $1
+lda $0
+again2:
+sta $0
+dex
+cpy #$0
 bne again
+
 hlt:
 jmp hlt
 rts
+
+
