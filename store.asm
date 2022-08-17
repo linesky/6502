2df463d9c3c910cda964b8f4851733d938933ec4
sky line <32512284+linesky@users.noreply.github.com>
2022-08-17 18:47:13 +0100

    fill mem sample code

--- store.asm
+++ store.asm
@@ -1,16 +1,59 @@
 ;http://www.6502asm.com/
-
-
+jmp _start
+_start:
 lda #$5
+sta $19
+lda #$2
+sta $16
+lda #$0
+sta $15
+lda #$3
+sta $18
+lda #$fa
+sta $17
+jsr fill
+jsr hlt
+
+;$15,16,address
+;$17,18,counter
+;$19,value
+fill:
+ldx $17
+cpx #$0
+bne fill11
+ldx $18
+cpx #$0
+beq fill10
+fill11:
 ldx #$0
-again:
-sta $200,x
-sta $300,x
-sta $400,x
-sta $500,x
+lda $19
+sta  ($15,x)
+ldx $15
 inx
-cmp $ff,x
-bne again
+cpx #$0
+bne fill2
+stx $15
+ldx $16
+inx
+stx $16
+ldx $15
+fill2:
+stx $15
+ldx $17
+dex
+stx $17
+cpx #$ff
+bne fill22
+ldx $18
+dex
+stx $18
+fill22:
+ldx $17
+cpx #$0
+bne fill11
+ldx $18
+cpx #$0
+bne fill11
+fill10:
 hlt:
 jmp hlt
-rts
